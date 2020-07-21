62188b849c44bf0f28f71aeba037cf032d49f70d
shichunjia <shichunjia@wondertek.com.cn>
2020-07-21 09:37:49 +0800

    rabbitmq

--- src/main/java/com/keeplearn/shichunjia/controller/RabbitController.java
+++ src/main/java/com/keeplearn/shichunjia/controller/RabbitController.java
@@ -3,21 +3,26 @@ package com.keeplearn.shichunjia.controller;
 
 import com.keeplearn.shichunjia.utils.MsgProducer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
-@RequestMapping("/RabbitMq/api")
-public class MabbitController {
+@RestController
+@RequestMapping("/api/rabbitmq")
+public class RabbitController {
 
     @Autowired
     private MsgProducer msgProducer;
 
 
-    @RequestMapping()
+    @GetMapping("/sent")
     public String sentMessage(String message){
         msgProducer.sendMsg("-------------111------------");
+        return "消息已发送";
+    }
+    @GetMapping("/test")
+    public String sentMessageTest(String message){
         return "消息已发送";
     }
 
